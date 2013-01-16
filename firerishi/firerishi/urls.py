985a330c0ec80e692d2d4b656695aed53067b21e
Thirumalaa Srinivas <srinivassekar@gmail.com>
2013-01-16 14:24:16 -0600

    first commit

---
+++ firerishi/firerishi/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+import os
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'firerishi.views.home', name='home'),
+    # url(r'^firerishi/', include('firerishi.foo.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(PROJECT_ROOT, "../static")}),
+		url(r'^$', 'landing.views.home'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
